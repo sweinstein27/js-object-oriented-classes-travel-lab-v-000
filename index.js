d0b0fad7f1242df38884b35c899f54b888a45183
Scott Weinstein <sweinstein27@gmail.com>
2018-10-29 15:32:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ class Driver {
     this.name = name;
     this.startDate = new Date(startDate);
   }
-  yearsExperienceFromBeginingof(){
+  yearsExperienceFromBeginningOf(year) {
     let endDate = new Date(year, 1, 1);
     let totalYears = (endDate - this.startDate) / (365 * 24 * 60 * 60 * 1000);
     return parseInt(totalYears);
   }
-  }
-
+}
 class Route {
   constructor(beginningLocation, endingLocation) {
     this.beginningLocation = beginningLocation;
